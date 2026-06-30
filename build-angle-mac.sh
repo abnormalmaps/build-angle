7156ef5f41392a751a1cb509af7bb58c7183ef93
abnormalmaps <abnormalmaps@gmail.com>
2026-06-29 22:11:05 -0400

    Fix install name for other builds

--- build-angle-mac.sh
+++ build-angle-mac.sh
@@ -99,6 +99,8 @@ for DYLIB in $DYLIBS; do
 done
 
 # Patch dylib names so that they get loaded locally
+install_name_tool -change libGLESv2.dylib @loader_path/libGLESv2.dylib build/mac/arm64/lib/libGLESv1_CM.dylib
+install_name_tool -change libGLESv2.dylib @loader_path/libGLESv2.dylib build/mac/x86_64/lib/libGLESv1_CM.dylib
 install_name_tool -change libGLESv2.dylib @loader_path/libGLESv2.dylib build/mac/universal/lib/libGLESv1_CM.dylib
 
 # Copy headers for all architectures
